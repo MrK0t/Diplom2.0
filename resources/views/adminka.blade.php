3d9d11db3ee6a2f1f8d026eadf06478699b62b89
MrK0t <vvvmr.kott@gmail.com>
2021-05-28 12:56:59 +0300

    start work with adminka

--- resources/views/adminka.blade.php
+++ resources/views/adminka.blade.php
@@ -1,3 +1,35 @@
 @extends('layouts.header')
 
 <h1>ADMINKA</h1>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Login') }}</div>
+
+                <div class="card-body">
+
+                    <form method="POST" action="{{ route('login') }}">
+
+                        <!-- foreach()
+                        {
+                            list
+                        } -->
+
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Login') }}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
