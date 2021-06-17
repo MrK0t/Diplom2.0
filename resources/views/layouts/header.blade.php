3288c69020e8ad2a81c0ce0893da804b2cf70112
MrK0t <vvvmr.kott@gmail.com>
2021-06-17 10:56:45 +0300

    lapt fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,23 +31,23 @@
 
 <body>
 <div class="py-3 bg-dark" style="margin-bottom: 80px;" >
-    <div class="row text-light">
+    <div class="row">
 
-        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-light">
             <div class="container-fluid">
-            <a href="/"><img src="/logo/logo.png" width="100" height="100"></a>
+            <a href="/"><img src="/logo/logo.svg" width="100" height="100"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="navbar-collapse collapse " id="navbarCollapse" style="">
                 <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-light" aria-current="page" href="{{ route('index')}}">Главная</a>
+                    <a class="nav-link active text-dark" aria-current="page" href="{{ route('index')}}">Главная</a>
                 </li>
                 @auth
                     @if(Auth::User()->isAdmin === 1)
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle text-dark" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Администрирование
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="Администрирование">
@@ -59,14 +59,14 @@
                         </ul>
                     </li>
                     @endif
-                    <a class="nav-link text-light" href="{{ route('profile.index')}}">Профиль</a>
+                    <a class="nav-link text-dark" href="{{ route('profile.index')}}">Профиль</a>
                 @endauth
                 @guest
-                    <a class="nav-link text-light" href="{{ route('login')}}">Вход</a>
-                    <a class="nav-link text-light" href="{{ route('register')}}">Регистрация</a>
+                    <a class="nav-link text-dark" href="{{ route('login')}}">Вход</a>
+                    <a class="nav-link text-dark" href="{{ route('register')}}">Регистрация</a>
                 @endguest
                 @auth
-                    <a class="nav-link text-light" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" href="{{ route('logout') }}">Выход({{Auth::user()->name}})</a>
+                    <a class="nav-link text-dark" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" href="{{ route('logout') }}">Выход({{Auth::user()->name}})</a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                     </form>
